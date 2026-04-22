be2b4a7d949d7a35f020d5768bd2df3f9f50a398
Sean <duansuhang1@gmail.com>
2026-04-22 20:12:44 +0100

    v0.1.8: release tray hover preview fix

--- src/App.tsx
+++ src/App.tsx
@@ -410,7 +410,7 @@ function App() {
 
             <div className="status-row">
               {loading ? 'Refreshing...' : `Updated: ${lastUpdated}`}
-              <span className="version-tag">v0.1.7</span>
+              <span className="version-tag">v0.1.8</span>
             </div>
 
             {!oauthStatus.available && (
